344b58aac51d165379ddc594c6179fe7d72b9895
Diogo <diogomrn@gmail.com>
2024-08-21 15:05:37 -0300

    Initial commit, base project, libraries and plugins configured.

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.baseline.profile)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.compose.compiler)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
+}
+
+android {
+    namespace = "tech.capitalcoding.pokedex"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "tech.capitalcoding.pokedex"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    composeCompiler {
+        enableStrongSkippingMode = true
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.hilt)
+    implementation(libs.navigation) // needed for Room
+    implementation(libs.room.ktx)
+    implementation(libs.timber)
+
+    implementation(libs.test.android.profile.installer)
+
+    ksp(libs.hilt.compiler)
+    ksp(libs.room.compiler)
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
