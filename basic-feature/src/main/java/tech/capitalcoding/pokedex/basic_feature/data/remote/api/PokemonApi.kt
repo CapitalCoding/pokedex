a1e3c122c205c121be2d5b930ddcd900e3c3004e
Diogo <diogomrn@gmail.com>
2024-08-23 08:14:09 -0300

    New api endpoints added

--- basic-feature/src/main/java/tech/capitalcoding/pokedex/basic_feature/data/remote/api/PokemonApi.kt
+++ basic-feature/src/main/java/tech/capitalcoding/pokedex/basic_feature/data/remote/api/PokemonApi.kt
@@ -2,9 +2,24 @@ package tech.capitalcoding.pokedex.basic_feature.data.remote.api
 
 import retrofit2.http.GET
 import tech.capitalcoding.pokedex.basic_feature.data.remote.model.pokemon.PokemonListResponse
+import tech.capitalcoding.pokedex.basic_feature.data.remote.model.pokemon.PokemonResponse
+import tech.capitalcoding.pokedex.basic_feature.data.remote.model.pokemon.PokemonTypeListResponse
+import tech.capitalcoding.pokedex.basic_feature.data.remote.model.pokemon.PokemonTypeResponse
 
 interface PokemonApi {
 
     @GET("pokemon")
-    suspend fun getPokemons(): List<PokemonListResponse>
+    suspend fun getPokemons(): PokemonListResponse
+
+    @GET("pokemon/{id}")
+    suspend fun getPokemonById(id: Int): PokemonResponse
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemonByName(name: String): PokemonResponse
+
+    @GET("type")
+    suspend fun getPokemonTypes(): PokemonTypeListResponse
+
+    @GET("type/{id}")
+    suspend fun getPokemonTypeByName(name: String): PokemonTypeResponse
 }
